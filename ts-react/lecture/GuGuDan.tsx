73796bf34814ed1904fd688b06b5bcc6dddd9225
dalsomin <dalsomin425@gmail.com>
2021-02-16 22:22:07 +0900

    ts start

---
+++ ts-react/lecture/GuGuDan.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useState, useRef } from 'react';
+//<>===React.Fragment
+const GuGuDan = () => {
+    const [first, setFirst] = useState(Math.ceil(Math.random() * 9))
+    const [second, setSecond] = useState(Math.ceil(Math.random() * 9))
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputEl = React.useRef(null);
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        const input = inputEl.current;
+        if (parseInt(value) === first * second) {
+            setResult('정답입니다~');
+            //정답이면 다시 문제내는 부분
+            setFirst(Math.ceil(Math.random() * 9));
+            setSecond(Math.ceil(Math.random() * 9));
+        }
+    }
+    return (
+        <>
+            <div>{first} 곱하기 {second} = ?</div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    ref={input}
+                    type="number"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+            </form>
+        </>
+    )
+}
+export default GuGuDan;
---
+++ ts-react/lecture/client.tsx
@@ -0,0 +1,8 @@
+//import React from 'react'; 
+//import ReactDOM from 'react-dom';
+import * as React from 'react';
+//react는 default 가 없기 때문에 이렇게 못쓴다. 
+import * as  ReactDOM from 'react-dom';
+import GuGuDan from './GuGuDan';
+
+ReactDOM.render(<GuGuDan />, document.querySelector("#root"));
